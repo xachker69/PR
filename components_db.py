a20cc7b5e441db9ec5f19b4c16e9717d6fb69935
Vlad <89689377+xachker69@users.noreply.github.com>
2025-06-24 01:22:45 +0300

    Add files via upload

---
+++ components_db.py
@@ -0,0 +1,115 @@
+COMPONENTS = {
+    'Для геймінгу': [
+        {
+            'name': 'Бюджетний',
+            'price': 24000,
+            'cpu': 'Intel Core i3-12100F',
+            'gpu': 'GeForce RTX 3050 8GB',
+            'ram': '16GB DDR4',
+            'storage': 'SSD 1TB',
+            'psu': '600W',
+            'case': 'Vinga CS311G',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_Hawk_A2241_I3M16G3050A2241-p854272.html'
+        },
+        {
+            'name': 'Оптимальний',
+            'price': 51000,
+            'cpu': 'AMD Ryzen 7 8700F',
+            'gpu': 'GeForce RTX 5060 Ti 16GB',
+            'ram': '32GB DDR5',
+            'storage': 'SSD 1TB',
+            'psu': '700W',
+            'motherboard': 'AMD B650',
+            'case': 'Vinga Gladius',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_Rhino_D9130_R7M32G5060TI16GD9130-p1237270.html'
+        },
+        {
+            'name': 'Максимальний',
+            'price': 115000,
+            'cpu': 'Intel Core Ultra 7 265K',
+            'gpu': 'GeForce RTX 5080 16GB',
+            'ram': '32GB DDR5',
+            'storage': 'SSD 1TB',
+            'psu': '850W',
+            'motherboard': 'Intel Z890',
+            'case': 'Vinga Linden',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_PBA_Odin_D8596_IU7M32G5080D8596-p1200553.html'
+        }
+    ],
+    'Для роботи з важкими програмами': [
+        {
+            'name': 'Бюджетний',
+            'price': 30000,
+            'cpu': 'AMD Ryzen 7 5700G',
+            'gpu': 'Radeon Graphics',
+            'ram': '32GB DDR4',
+            'storage': 'SSD 1TB',
+            'psu': '500W',
+            'motherboard': 'AMD A520',
+            'case': 'Vinga Ghost',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_Advanced_D8427_R7M32INTWPD8427-p1176056.html'
+        },
+        {
+            'name': 'Оптимальний',
+            'price': 45000,
+            'cpu': 'AMD Ryzen 5 7500F',
+            'gpu': 'GeForce RTX 5060 Ti 8GB',
+            'ram': '16GB DDR5',
+            'storage': 'SSD 512GB',
+            'psu': '700W',
+            'motherboard': 'AMD B650',
+            'case': 'Vinga Gladius',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_Rhino_D9076_R5M16G5060TI8GD9076-p1236941.html'
+        },
+        {
+            'name': 'Максимальний',
+            'price': 65000,
+            'cpu': 'Intel Core i7, 12700KF',
+            'gpu': 'GeForce RTX 5060 Ti 16GB',
+            'ram': '32GB DDR5',
+            'storage': 'SSD 2TB',
+            'psu': '700W',
+            'motherboard': 'Intel B760',
+            'case': 'Vinga Gladius',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_Wolverine_D9026_I7M32G5060TI16GWHD9026-p1236891.html'
+        }
+    ],
+    'Для офісних задач': [
+        {
+            'name': 'Бюджетний',
+            'price': 10000,
+            'cpu': 'Intel Core i3-10100',
+            'gpu': 'UHD Graphics 630',
+            'ram': '8GB DDR4',
+            'storage': 'SSD 240GB',
+            'psu': '400W',
+            'motherboard': 'Intel H410',
+            'case': 'Vinga CS108B',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_Advanced_A0196_I3M8INTA0196-p711268.html'
+        },
+        {
+            'name': 'Оптимальний',
+            'price': 15000,
+            'cpu': 'Intel Core i5-12400',
+            'gpu': 'Intel UHD 730',
+            'ram': '8GB DDR4',
+            'storage': 'SSD 480GB',
+            'psu': '500W',
+            'motherboard': 'Intel H610',
+            'case': 'Vinga Smart-500W',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_Advanced_D0073_I5M8INTD0073-p956026.html'
+        },
+        {
+            'name': 'Максимальний',
+            'price': 20000,
+            'cpu': 'AMD Ryzen 5 5600G',
+            'gpu': 'Radeon Vega 7',
+            'ram': '32GB DDR4',
+            'storage': 'SSD 1TB',
+            'psu': '500W',
+            'motherboard': 'AMD B550',
+            'case': 'Vinga CS311G',
+            'link': 'https://brain.com.ua/ukr/Komp_yuter_Vinga_Advanced_B0085_R5M32INTB0085-p793522.html'
+        }
+    ]
+}
